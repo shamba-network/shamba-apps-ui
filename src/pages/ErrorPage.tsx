fbe00fc24201b27b92119508f69edad01ff919aa
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2022-11-23 09:31:17 +0300

    SEDO-86 Implement error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,16 +1,19 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { SmileOutlined } from '@ant-design/icons'
+import { Button } from 'antd';
 
 const ErrorPage = () => {
-//   const error = useRouteError();
-//   console.error(error);
+  const navigate = useNavigate()
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        {/* <i>{error.statusText || error.message}</i> */}
-      </p>
+    <div id="error-page" className="error-page">
+      <SmileOutlined rotate={180} style={{ fontSize: '40px' }} />
+      <p style={{ marginTop: '15px' }}>Sorry, an unexpected error has occurred.</p>
+      <Button
+        onClick={() => navigate("/")}
+      >
+        Go Home
+      </Button>
     </div>
   );
 }
