f8535ed451e91feca289ea2f3ad7646f38fa5097
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2024-01-22 16:24:23 +0300

    Update auth implementation

--- src/index.tsx
+++ src/index.tsx
@@ -59,8 +59,10 @@ root.render(
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH_DOMAIN!}
       clientId={process.env.REACT_APP_AUTH_CLIENT_ID!}
-      redirectUri={`${window.location.origin}/apps`}
-      audience={process.env.REACT_APP_API_AUDIENCE}
+      authorizationParams={{
+        redirect_uri: `${window.location.origin}/apps`,
+        audience: process.env.REACT_APP_API_AUDIENCE
+      }}
     >
       <RouterProvider router={router} />
     </Auth0Provider>
